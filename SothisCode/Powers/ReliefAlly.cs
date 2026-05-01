9fa498c799da82731e3e8dd2253345fd2c163227
cssmith4509 <cssmith4509@gmail.com>
2026-04-30 19:54:36 -0500

    relief power artwork

--- SothisCode/Powers/ReliefAlly.cs
+++ SothisCode/Powers/ReliefAlly.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
+using Sothis.SothisCode.Extensions;
 
 namespace Sothis.SothisCode.Powers;
 
@@ -12,6 +13,9 @@ public class ReliefAlly : SothisPower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override string CustomPackedIconPath => "relief.png".PowerImagePath();
+    public override string CustomBigIconPath => "relief.png".BigPowerImagePath();
     
     public override async Task BeforeTurnEndEarly(PlayerChoiceContext choiceContext, CombatSide side)
     {
--- SothisCode/Powers/ReliefEnemy.cs
+++ SothisCode/Powers/ReliefEnemy.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
+using Sothis.SothisCode.Extensions;
 
 namespace Sothis.SothisCode.Powers;
 
@@ -12,6 +13,9 @@ public class ReliefEnemy : SothisPower
 {
     public override PowerType Type => PowerType.Debuff;
     public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override string CustomPackedIconPath => "relief.png".PowerImagePath();
+    public override string CustomBigIconPath => "relief.png".BigPowerImagePath();
 
     public override async Task AfterTurnEndLate(PlayerChoiceContext choiceContext, CombatSide side)
     {
